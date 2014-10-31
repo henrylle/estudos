6a511dc1c7848f4eb761851d9ea027699a8e09ff
Pedro Benevides <pedrobenevidesb@gmail.com>
2014-10-31 13:44:48 -0300

    Configurado connection string modo Release Configurado rota pardrão para controller Convidados E adicionando EF no projeto Web via NuGet

--- Casando.Web/App_Start/RouteConfig.cs
+++ Casando.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Casando.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Convidados", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
